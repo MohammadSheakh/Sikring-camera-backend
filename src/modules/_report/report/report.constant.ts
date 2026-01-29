630416002d6b64e1c7102ded40088ea66d98e61c
MohammadSheakh <mohammad.sheakh@gmail.com>
2026-01-29 17:41:56 +0600

    report constant changed emergency_call_out changed to mast_relocation_report

--- src/modules/_report/report/report.constant.ts
+++ src/modules/_report/report/report.constant.ts
@@ -2,7 +2,8 @@ export enum TReportType {
   alarmPatrol = 'alarmPatrol',
   patrolReport = 'patrolReport',
   service = 'service',
-  emergency_call_out = 'emergency_call_out',
+  emergency_call_out = 'mast_relocation_report', //🆕
+  // previously emergency_call_out .. now 
 }
 
 export enum TIncidentSevearity {
